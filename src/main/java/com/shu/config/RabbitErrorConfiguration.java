8944dd64f588c17318f7fba0b3a2b02cf6bfce6e
YHEMPR <851314610@qq.com>
2024-06-23 16:13:43 +0800

    为rabbitmq添加软件错误队列，处理错误邮箱注册

---
+++ src/main/java/com/shu/config/RabbitErrorConfiguration.java
@@ -0,0 +1,42 @@
+package com.shu.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.retry.MessageRecoverer;
+import org.springframework.amqp.rabbit.retry.RepublishMessageRecoverer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+/**
+ * @author 85131
+ */
+@Configuration
+@ConditionalOnProperty(prefix = "spring.rabbitmq.listener.simple.retry",name = "enable",havingValue = "true")
+public class RabbitErrorConfiguration {
+
+    @Bean
+    public DirectExchange errorExchange() {
+        return new DirectExchange("error.direct");
+    }
+
+    @Bean
+    public Queue errorQueue() {
+        return new Queue("error.queue");
+    }
+
+    @Bean
+    public Binding errorBinding(Queue errorQueue,DirectExchange errorExchange) {
+        return BindingBuilder.bind(errorQueue).to(errorExchange).with("error");
+    }
+
+    @Bean
+    public MessageRecoverer messageRecoverer(RabbitTemplate rabbitTemplate) {
+        return new RepublishMessageRecoverer(rabbitTemplate,"error.direct","error");
+    }
+}
