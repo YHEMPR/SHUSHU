61d82bf32a0461ef88d9c4fb06e49ac40979d48a
YHEMPR <851314610@qq.com>
2024-06-28 00:04:53 +0800

    功能（rabbitmq）：添加错误处理的重试机制
    添加在发生错误时重试RabbitMQ消息的配置。包括设置错误交换器、错误队列，并将它们绑定在一起以便处理可重试的消息。同时，引入RepublishMessageRecoverer，将消息重新发送到专门的错误处理交换器。

--- src/main/java/com/shu/config/RabbitErrorConfiguration.java
+++ src/main/java/com/shu/config/RabbitErrorConfiguration.java
@@ -11,30 +11,54 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
-
 /**
- * @author 85131
+ * 配置类，用于在RabbitMQ中处理错误消息的重试机制。
+ * 只有在配置属性spring.rabbitmq.listener.simple.retry.enable为true时，才会启用此配置。
  */
 @Configuration
 @ConditionalOnProperty(prefix = "spring.rabbitmq.listener.simple.retry",name = "enable",havingValue = "true")
 public class RabbitErrorConfiguration {
 
+    /**
+     * 定义一个名为error.direct的Direct类型交换机，用于处理错误消息。
+     *
+     * @return DirectExchange
+     */
     @Bean
     public DirectExchange errorExchange() {
         return new DirectExchange("error.direct");
     }
 
+    /**
+     * 定义一个名为error.queue的队列，用于存储错误消息。
+     *
+     * @return Queue
+     */
     @Bean
     public Queue errorQueue() {
         return new Queue("error.queue");
     }
 
+    /**
+     * 将error.queue队列绑定到error.direct交换机上，路由键为error。
+     * 这样，所有路由到error.direct交换机并匹配路由键error的消息都将被投递到error.queue队列中。
+     *
+     * @param errorQueue 错误消息队列
+     * @param errorExchange 处理错误消息的交换机
+     * @return Binding
+     */
     @Bean
-    public Binding errorBinding(Queue errorQueue,DirectExchange errorExchange) {
+    public Binding errorBinding(Queue errorQueue, DirectExchange errorExchange) {
         return BindingBuilder.bind(errorQueue).to(errorExchange).with("error");
     }
 
+    /**
+     * 创建一个RepublishMessageRecoverer实例，用于在消息处理失败时重新发布消息到error.exchange交换机。
+     * 通过这种方式，错误消息可以被重新路由到一个专门处理错误的队列或消费者。
+     *
+     * @param rabbitTemplate RabbitMQ模板，用于发送消息
+     * @return MessageRecoverer
+     */
     @Bean
     public MessageRecoverer messageRecoverer(RabbitTemplate rabbitTemplate) {
         return new RepublishMessageRecoverer(rabbitTemplate,"error.direct","error");
