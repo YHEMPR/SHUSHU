ca5781ad549abccbf1f576f972498ebd9d35d49e
YHEMPR <851314610@qq.com>
2024-06-28 07:19:38 +0800

    WebConfiguration类中添加BCryptPasswordEncoder的Bean定义，以支持使用BCrypt算法进行密码加密。

--- src/main/java/com/shu/config/WebConfiguration.java
+++ src/main/java/com/shu/config/WebConfiguration.java
@@ -7,13 +7,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
- * 一般Web服务相关配置
+ * Web配置类，用于配置Spring Web MVC和安全相关的设置。
  */
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
+    /**
+     * 配置密码编码器。
+     *
+     * @return 返回一个BCryptPasswordEncoder实例，用于加密和验证用户密码。
+     * 使用BCrypt算法对密码进行加密，提高系统安全性。
+     */
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 }
+
